4f7cc4723602406fe6410b4c8b1e7eecc73cb09e
LydiaDabbah <99916438+LydiaDabbah@users.noreply.github.com>
2022-02-17 18:58:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let pokemonArray = [];
-const json_file = JSON.parse(
-  '{"results":[{"name":"Luke Skywalker","height":"172","mass":"77","hair_color":"blond","skin_color":"fair","eye_color":"blue","birth_year":"19BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":["https://swapi.dev/api/vehicles/14/","https://swapi.dev/api/vehicles/30/"],"starships":["https://swapi.dev/api/starships/12/","https://swapi.dev/api/starships/22/"],"created":"2014-12-09T13:50:51.644000Z","edited":"2014-12-20T21:17:56.891000Z","url":"https://swapi.dev/api/people/1/"},{"name":"C-3PO","height":"167","mass":"75","hair_color":"n/a","skin_color":"gold","eye_color":"yellow","birth_year":"112BBY","gender":"n/a","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/4/","https://swapi.dev/api/films/5/","https://swapi.dev/api/films/6/"],"species":["https://swapi.dev/api/species/2/"],"vehicles":[],"starships":[],"created":"2014-12-10T15:10:51.357000Z","edited":"2014-12-20T21:17:50.309000Z","url":"https://swapi.dev/api/people/2/"},{"name":"R2-D2","height":"96","mass":"32","hair_color":"n/a","skin_color":"white, blue","eye_color":"red","birth_year":"33BBY","gender":"n/a","homeworld":"https://swapi.dev/api/planets/8/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/4/","https://swapi.dev/api/films/5/","https://swapi.dev/api/films/6/"],"species":["https://swapi.dev/api/species/2/"],"vehicles":[],"starships":[],"created":"2014-12-10T15:11:50.376000Z","edited":"2014-12-20T21:17:50.311000Z","url":"https://swapi.dev/api/people/3/"},{"name":"Darth Vader","height":"202","mass":"136","hair_color":"none","skin_color":"white","eye_color":"yellow","birth_year":"41.9BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":[],"starships":["https://swapi.dev/api/starships/13/"],"created":"2014-12-10T15:18:20.704000Z","edited":"2014-12-20T21:17:50.313000Z","url":"https://swapi.dev/api/people/4/"},{"name":"Leia Organa","height":"150","mass":"49","hair_color":"brown","skin_color":"light","eye_color":"brown","birth_year":"19BBY","gender":"female","homeworld":"https://swapi.dev/api/planets/2/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":["https://swapi.dev/api/vehicles/30/"],"starships":[],"created":"2014-12-10T15:20:09.791000Z","edited":"2014-12-20T21:17:50.315000Z","url":"https://swapi.dev/api/people/5/"},{"name":"Owen Lars","height":"178","mass":"120","hair_color":"brown, grey","skin_color":"light","eye_color":"blue","birth_year":"52BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/5/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":[],"starships":[],"created":"2014-12-10T15:52:14.024000Z","edited":"2014-12-20T21:17:50.317000Z","url":"https://swapi.dev/api/people/6/"},{"name":"Beru Whitesun lars","height":"165","mass":"75","hair_color":"brown","skin_color":"light","eye_color":"blue","birth_year":"47BBY","gender":"female","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/5/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":[],"starships":[],"created":"2014-12-10T15:53:41.121000Z","edited":"2014-12-20T21:17:50.319000Z","url":"https://swapi.dev/api/people/7/"},{"name":"R5-D4","height":"97","mass":"32","hair_color":"n/a","skin_color":"white, red","eye_color":"red","birth_year":"unknown","gender":"n/a","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/"],"species":["https://swapi.dev/api/species/2/"],"vehicles":[],"starships":[],"created":"2014-12-10T15:57:50.959000Z","edited":"2014-12-20T21:17:50.321000Z","url":"https://swapi.dev/api/people/8/"},{"name":"Biggs Darklighter","height":"183","mass":"84","hair_color":"black","skin_color":"light","eye_color":"brown","birth_year":"24BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/"],"species":[],"vehicles":[],"starships":["https://swapi.dev/api/starships/12/"],"created":"2014-12-10T15:59:50.509000Z","edited":"2014-12-20T21:17:50.323000Z","url":"https://swapi.dev/api/people/9/"},{"name":"Obi-Wan Kenobi","height":"182","mass":"77","hair_color":"auburn, white","skin_color":"fair","eye_color":"blue-gray","birth_year":"57BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/20/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/4/","https://swapi.dev/api/films/5/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":["https://swapi.dev/api/vehicles/38/"],"starships":["https://swapi.dev/api/starships/48/","https://swapi.dev/api/starships/59/","https://swapi.dev/api/starships/64/","https://swapi.dev/api/starships/65/","https://swapi.dev/api/starships/74/"],"created":"2014-12-10T16:16:29.192000Z","edited":"2014-12-20T21:17:50.325000Z","url":"https://swapi.dev/api/people/10/"}]}'
-);
-//console.log(json_file);
+
 
 const poke_file = JSON.parse(` [{
 			"abilities": [
@@ -3224,84 +3221,6 @@ const poke_file = JSON.parse(` [{
   ]
 `);
 
-class Node {
-  constructor(data, info) {
-    this.info = info;
-    this.data = data;
-    this.left = null; // this.next
-    this.right = null; // this.next2
-  }
-}
-
-class BinaryTree {
-  constructor(root = null) {
-    this.root = root;
-  }
-
-  // metodos...
-  insert(data, info) {
-    let node = new Node(data, info);
-
-    if (this.root == null) {
-      this.root = node;
-    } else {
-      let current = this.root;
-      let prev;
-
-      while (current != null) {
-        prev = current;
-        if (node.data < current.data) {
-          current = current.left;
-        } else {
-          current = current.right;
-        }
-      }
-
-      if (node.data < prev.data) {
-        prev.left = node;
-      } else {
-        prev.right = node;
-      }
-    }
-  }
-
-  search(data) {
-    if (this.root.data == data) {
-      return this.root;
-    } else {
-      let current = this.root;
-
-      while (current !== null) {
-        if (current.data == data) break;
-
-        if (data < current.data) {
-          current = current.left;
-        } else {
-          current = current.right;
-        }
-      }
-
-      return current;
-    }
-  }
-
-  preOrder() {
-    var result = [];
-
-    return this.preOrderHelper(this.root, result);
-  }
-
-  preOrderHelper(root, result) {
-    if (root !== null) {
-      result.push(root.data);
-      this.preOrderHelper(root.left, result);
-      this.preOrderHelper(root.right, result);
-    }
-
-    return result;
-  }
-}
-
 class Pokemon {
   constructor(id, name, img, type, weight, height, abilities, weakness) {
     this.id = id;
@@ -3315,25 +3234,22 @@ class Pokemon {
   }
 }
 
-//crearArray();
-for (i = 0; i < pokemonArray.length; i++) {
-  console.log(pokemonArray[i].id);
-}
 
-function insertionSort(inputArr) {
-  let n = inputArr.length;
-  for (let i = 1; i < n; i++) {
-    // Choosing the first element in our unsorted subarray
-    let current = inputArr[i];
-    // The last element of our sorted subarray
+//ordenar
+function insertionSort(array) {
+  
+  for (let i = 1; i < array.length; i++) {
+    
+    let current = array[i];
+  
     let j = i - 1;
-    while (j > -1 && current.name < inputArr[j].name) {
-      inputArr[j + 1] = inputArr[j];
+    while (j > -1 && current.name < array[j].name) {
+      array[j + 1] = array[j];
       j--;
     }
-    inputArr[j + 1] = current;
+    array[j + 1] = current;
   }
-  return inputArr;
+  return array;
 }
 
 function crearArray() {
